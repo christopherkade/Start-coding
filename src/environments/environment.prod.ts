5077d2e1e0ef14a4e2cbed5ebe5165fe43881b9f
christopher <christopher.kade@epitech.eu>
2017-11-01 16:17:07 +0000

    Added Firebase info in our prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCl878798PoiJREDcxc-LFQGfHC_51NWo0',
+    authDomain: 'start-coding.firebaseapp.com',
+    databaseURL: 'https://start-coding.firebaseio.com',
+    projectId: 'start-coding',
+    storageBucket: '',
+    messagingSenderId: '948468266810'
+  }
 };
